8431cb5e7d1792da02d896dfd704a8c827e0ebe4
gongwq119 <wq.gong119@gmail.com>
2013-03-26 18:08:59 +0800

    a

--- com.gongwq.php1/web/index.php
+++ com.gongwq.php1/web/index.php
@@ -1,6 +1,9 @@
 <?php
 require '../controller/demoContr.php';
+require '../db/DataAccess.php';
 echo 'hello world';
 echo '<br/>';
-$demoController = new demoContr();
-$demoController->index();
+// $demoController = new demoContr();
+// $demoController->index();
+$db = new DataAccess('localhost', 'root', '1', 'php');
+$db->getRow("select * from `php`.`users`");
