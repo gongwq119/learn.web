ec0e80d90991dcc27060674baf0da94922900b8b
gongwq119 <wq.gong119@gmail.com>
2013-05-14 12:04:49 +0800

    try to sync with github,mac'local code is the newest

--- com.gongwq.php1/view/simpleView.php
+++ com.gongwq.php1/view/simpleView.php
@@ -12,3 +12,24 @@ class simpleView {
 		echo $this->outputHtml;
 	}
 }
+<?php
+class simpleView {
+	
+	private $model;
+	private $htmlOutput; //the html code
+	
+	/**
+	 * the constructor
+	 * @param unknown $model
+	 */
+	function __construct($model) {
+		$this->model = $model;
+	}
+	
+	/**
+	 * Output the final html code.
+	 */
+	function display() {
+		echo $this->htmlOutput;
+	}
+}
