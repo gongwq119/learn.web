459d72d813051ffe7ac30501d2e7a9a670e32a65
gongwq119home <gongwq119@hotmail.com>
2013-06-20 17:43:36 +0800

    modify the db script name and add restore.sh

---
+++ com.gongwq.php1/db/restore.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#Run the mysqldump to backup the db
+if [ -f "/Applications/XAMPP/xamppfiles/bin/mysqldump" ];then
+echo "This is Macbook computer"
+/Applications/XAMPP/xamppfiles/bin/mysql -uroot -ppassw0rd mydb < /Users/wenqianggong/git/learn.web/com.gongwq.php1/db/backup.sql
+echo "restore db is done!!!"
+elif [ -f "/opt/lampp/bin/mysqldump" ];then
+echo "This is x61 computer"
+/opt/lampp/bin/mysql -uroot -ppassw0rd mydb < /home/gongwq/site/php1/com.gongwq.php1/db/backup.sql
+echo "restore db is done!!!"
+fi
