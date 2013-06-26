1cd44d3186837c31ef20fd0cd37b56c252b6755a
gongwq119 <wq.gong119@gmail.com>
2013-06-26 18:13:56 +0800

    update the db

--- com.gongwq.php1/db/backup.sql
+++ com.gongwq.php1/db/backup.sql
@@ -90,14 +90,15 @@ CREATE TABLE `items` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `sn` varchar(45) NOT NULL,
   `name` varchar(45) NOT NULL,
-  `price` decimal(10,0) NOT NULL,
+  `price` decimal(10,0) NOT NULL DEFAULT '0',
   `description` text NOT NULL,
-  `cat_id` int(10) unsigned NOT NULL,
-  `brand_id` int(10) unsigned NOT NULL,
-  `click_count` int(11) NOT NULL,
+  `cat_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `brand_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `click_count` int(11) NOT NULL DEFAULT '0',
+  `quantity` int(10) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`,`name`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=44 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -106,7 +107,7 @@ CREATE TABLE `items` (
 
 LOCK TABLES `items` WRITE;
 /*!40000 ALTER TABLE `items` DISABLE KEYS */;
-INSERT INTO `items` VALUES (1,'000001','不锈钢按钮',3,'按钮按钮',1,0,0),(2,'000002','控制主板',1990,'三菱主板',2,0,0),(3,'000003','压克力按钮',3,'按钮按钮',1,0,0);
+INSERT INTO `items` VALUES (1,'000001','不锈钢按钮',3,'按钮按钮',1,0,0,0),(2,'000002','控制主板',1990,'三菱主板',2,0,0,0),(3,'000003','压克力按钮',3,'按钮按钮',1,0,0,0),(4,'','苹果',1,'ffff',2,0,0,0),(5,'','鸭梨',2,'ffff',1,0,0,0),(6,'','鼠标',3,'ffff',2,0,0,0),(7,'','键盘',234,'ffff',1,0,0,0),(8,'','水杯',56,'ffff',2,0,0,0),(9,'','卫生纸',78,'ffff',1,0,0,0),(10,'','激光测距仪',1223,'ffff',2,0,0,0),(11,'','油笔',1,'ffff',1,0,0,0),(12,'','笔记本',0,'ffff',2,0,0,0),(13,'','',0,'ffff',1,0,0,0),(14,'','苹果',1,'ffff',2,0,0,0),(15,'','鸭梨',2,'ffff',1,0,0,0),(16,'','鼠标',3,'ffff',2,0,0,0),(17,'','键盘',234,'ffff',1,0,0,0),(18,'','水杯',56,'ffff',2,0,0,0),(19,'','卫生纸',78,'ffff',1,0,0,0),(20,'','激光测距仪',1223,'ffff',2,0,0,0),(21,'','油笔',1,'ffff',1,0,0,0),(22,'','笔记本',0,'ffff',2,0,0,0),(23,'','',0,'ffff',1,0,0,0),(24,'000003','苹果',1,'ffff',2,0,0,0),(25,'','鼠标',3,'ffff',2,0,0,0),(26,'','键盘',234,'ffff',1,0,0,0),(27,'','水杯',56,'ffff',2,0,0,0),(28,'','卫生纸',78,'ffff',1,0,0,0),(29,'','激光测距仪',1223,'ffff',2,0,0,0),(30,'','油笔',1,'ffff',1,0,0,0),(31,'','笔记本',0,'ffff',2,0,0,0),(32,'','你没',12,'ffff',1,0,0,0),(33,'','haohao',190,'ffff',2,0,0,0),(34,'000003','苹果',1,'ffff',2,0,0,0),(35,'000005','鼠标',3,'ffff',2,0,0,0),(36,'000006','键盘',234,'ffff',1,0,0,0),(37,'000007','水杯',56,'ffff',2,0,0,0),(38,'000008','卫生纸',78,'ffff',1,0,0,0),(39,'000009','激光测距仪',1223,'ffff',2,0,0,0),(40,'000010','油笔',1,'ffff',1,0,0,0),(41,'000011','笔记本',0,'ffff',2,0,0,0),(42,'000012','你没',12,'ffff',1,0,0,0),(43,'','haohao',190,'ffff',2,0,0,0);
 /*!40000 ALTER TABLE `items` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -119,4 +120,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-06-20 17:28:04
+-- Dump completed on 2013-06-26 18:13:20
