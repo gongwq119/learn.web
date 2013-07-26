34f47b26b2d929bf9bc9015385cacae0fa7333a4
gongwq119 <wq.gong119@gmail.com>
2013-07-26 14:00:20 +0800

    delete the unused folder

--- com.gongwq.php1/controller/demoContr.php
+++
@@ -1,6 +0,0 @@
-<?php
-class demoContr {
-	function index() {
-		echo "This controller works perfect";
-	}
-}
--- com.gongwq.php1/web/test/im.php
+++ com.gongwq.php1/web/test/im.php
@@ -1,11 +1,6 @@
 
 <?php
-    function some_other_func($a, $b)
-    {
-        $param = func_get_args();
-        $param = join(", ", $param);    // 分解显示
 
-        echo "the arglist is: $param\n";
-    }
-    some_other_func(1,3,5,7,9);
+    echo "hello";
+    echo "jjj";
 ?>
