36f291bad6ff3e9fb69aff3615bd0623b2be07ad
gongwq119 <wq.gong119@gmail.com>
2013-08-02 17:43:25 +0800

    update db

--- com.gongwq.php1/db/backup.sql
+++ com.gongwq.php1/db/backup.sql
@@ -96,7 +96,7 @@ CREATE TABLE `categories` (
   `is_show` tinyint(1) unsigned NOT NULL DEFAULT '1',
   PRIMARY KEY (`cat_id`),
   KEY `parent_id` (`parent_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -105,7 +105,7 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (6,'电梯主板','主板','',0,0,1),(7,'外招按钮','按钮','',0,0,1);
+INSERT INTO `categories` VALUES (6,'电梯主板','主板','',0,0,1),(7,'外招按钮','按钮','',0,0,1),(9,'测试类','测试','<p>没有没有</p>',7,0,1);
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -126,9 +126,10 @@ CREATE TABLE `items` (
   `brand_id` int(10) unsigned NOT NULL DEFAULT '0',
   `click_count` int(11) NOT NULL DEFAULT '0',
   `it_quant` int(10) NOT NULL DEFAULT '0',
+  `img_id` mediumint(8) NOT NULL,
   PRIMARY KEY (`it_id`),
   UNIQUE KEY `id_UNIQUE` (`it_id`,`it_name`)
-) ENGINE=MyISAM AUTO_INCREMENT=53 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=82 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -137,7 +138,7 @@ CREATE TABLE `items` (
 
 LOCK TABLES `items` WRITE;
 /*!40000 ALTER TABLE `items` DISABLE KEYS */;
-INSERT INTO `items` VALUES (1,'000001','不锈钢按钮',3,'按钮按钮',1,0,0,0),(2,'000002','控制主板',1990,'三菱主板',2,0,0,0),(3,'000003','压克力按钮',3,'按钮按钮',1,0,0,0),(4,'','苹果',1,'ffff',2,0,0,0),(5,'','鸭梨',2,'ffff',1,0,0,0),(6,'','鼠标',3,'ffff',2,0,0,0),(7,'','键盘',234,'ffff',1,0,0,0),(8,'','水杯',56,'ffff',2,0,0,0),(9,'','卫生纸',78,'ffff',1,0,0,0),(10,'','激光测距仪',1223,'ffff',2,0,0,0),(11,'','油笔',1,'ffff',1,0,0,0),(12,'','笔记本',0,'ffff',2,0,0,0),(13,'','',0,'ffff',1,0,0,0),(14,'','苹果',1,'ffff',2,0,0,0),(15,'','鸭梨',2,'ffff',1,0,0,0),(16,'','鼠标',3,'ffff',2,0,0,0),(17,'','键盘',234,'ffff',1,0,0,0),(18,'','水杯',56,'ffff',2,0,0,0),(19,'','卫生纸',78,'ffff',1,0,0,0),(20,'','激光测距仪',1223,'ffff',2,0,0,0),(21,'','油笔',1,'ffff',1,0,0,0),(22,'','笔记本',0,'ffff',2,0,0,0),(23,'','',0,'ffff',1,0,0,0),(24,'000003','苹果',1,'ffff',2,0,0,0),(25,'','鼠标',3,'ffff',2,0,0,0),(26,'','键盘',234,'ffff',1,0,0,0),(27,'','水杯',56,'ffff',2,0,0,0),(28,'','卫生纸',78,'ffff',1,0,0,0),(29,'','激光测距仪',1223,'ffff',2,0,0,0),(30,'','油笔',1,'ffff',1,0,0,0),(31,'','笔记本',0,'ffff',2,0,0,0),(32,'','你没',12,'ffff',1,0,0,0),(33,'','haohao',190,'ffff',2,0,0,0),(34,'000003','苹果',1,'ffff',2,0,0,0),(35,'000005','鼠标',3,'ffff',2,0,0,0),(36,'000006','键盘',234,'ffff',1,0,0,0),(37,'000007','水杯',56,'ffff',2,0,0,0),(38,'000008','卫生纸',78,'ffff',1,0,0,0),(39,'000009','激光测距仪',1223,'ffff',2,0,0,0),(40,'000010','油笔',1,'ffff',1,0,0,0),(41,'000011','笔记本',0,'ffff',2,0,0,0),(42,'000012','你没',12,'ffff',1,0,0,0),(43,'','haohao',190,'ffff',2,0,0,0),(44,'123','daf',123,'0',1,0,0,0),(45,'123','tttt',123,'0',1,0,0,0),(46,'123','ggg',123,'0',1,0,0,0),(47,'123','fdsa',123,'0',1,0,0,0),(48,'321','fdsadfasfasd',123,'0',1,0,0,0),(49,'123','fdsfa',123,'0',2,0,0,0),(50,'123','asdfasdfasdf',123,'0',1,0,0,0),(51,'123','dfdfdf',123,'0',1,0,0,0),(52,'asd','fgfdgs',0,'0',0,0,0,0);
+INSERT INTO `items` VALUES (1,'000001','不锈钢按钮',3,'按钮按钮',1,0,0,0,0),(2,'000002','控制主板',1990,'三菱主板',2,0,0,0,0),(80,'yyy','yyy',21,'<p>请编辑商品描述</p>',6,1,0,6,60),(81,'00000023','平层感应器',123,'<p>平层感应器，为宁波生产</p><p>使用品牌为<strong>otis</strong>，<strong>kone</strong>，<strong>shmit</strong>等等</p><p><br/></p>',6,2,0,1000,0);
 /*!40000 ALTER TABLE `items` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -155,7 +156,7 @@ CREATE TABLE `items_images` (
   `thumb_url` varchar(255) NOT NULL DEFAULT '',
   `large_url` varchar(255) NOT NULL,
   PRIMARY KEY (`img_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=61 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -164,7 +165,7 @@ CREATE TABLE `items_images` (
 
 LOCK TABLES `items_images` WRITE;
 /*!40000 ALTER TABLE `items_images` DISABLE KEYS */;
-INSERT INTO `items_images` VALUES (1,50,'/upload/201307/stand/1373528053342149899.jpg','/upload/201307/thumb/1373528053366480464.jpg','/upload/201307/large/1373528053867159003.jpg');
+INSERT INTO `items_images` VALUES (60,80,'/upload/201307/stand/1374467683800549716.jpg','/upload/201307/thumb/1374467683285674089.jpg','/upload/201307/large/1374467683232423320.jpg'),(59,80,'/upload/201307/stand/1374467667507070614.jpg','/upload/201307/thumb/1374467667329674125.jpg','/upload/201307/large/1374467668133814030.jpg');
 /*!40000 ALTER TABLE `items_images` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -177,4 +178,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-07-15 18:25:02
+-- Dump completed on 2013-08-02 17:43:01
